190012d37e89eba9d563546e706bc090524fe100
nerellamanoj <nerellamanojkumar@gmail.com>
2024-04-20 13:05:32 +0530

    react fakestorefiles

---
+++ App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import Manoj from './components/functional/first';
+import ManojKumar from './components/fakestore';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+     <ManojKumar/>
+
+
+
+
+
+
+
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ components/functional/first.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+var a = {
+  URL: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  dimensions: 100,
+  alt: "manoj",
+};
+ 
+var {URL,dimensions,alt}=a; 
+
+function Manoj() {
+   return(
+    <>
+
+    <img src={URL} width={dimensions} alt={Manoj}/>
+    
+    
+    
+    
+    </>
+
+
+
+
+
+
+   )
+}
+
+export default Manoj;
